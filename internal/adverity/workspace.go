b3d2bc3d924a003081830beb2b99ceca87c7ffa7
Thomas Nowotny <8328255+thomasnowotny@users.noreply.github.com>
2025-04-27 00:29:24 +0200

    Implement workspace support for Adverity client

---
+++ internal/adverity/workspace.go
@@ -0,0 +1,140 @@
+package adverity
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/url"
+)
+
+type CreateWorkspaceRequest struct {
+	DatalakeID int    `json:"datalake_id,omitempty" url:"datalake_id,omitempty"`
+	ParentID   int    `json:"parent_id,omitempty"   url:"parent_id,omitempty"`
+	Name       string `json:"name,omitempty"        url:"name,omitempty"`
+}
+
+type ReadWorkspaceRequest struct {
+	StackSlug string `json:"stack_slug,omitempty" url:"stack_slug,omitempty"`
+}
+
+type UpdateWorkspaceRequest struct {
+	StackSlug  string `json:"stack_slug,omitempty"  url:"stack_slug,omitempty"`
+	DatalakeID int    `json:"datalake_id,omitempty" url:"datalake_id,omitempty"`
+	ParentID   int    `json:"parent_id,omitempty"   url:"parent_id,omitempty"`
+	Name       string `json:"name,omitempty"        url:"name,omitempty"`
+}
+
+type DeleteWorkspaceRequest struct {
+	StackSlug string `json:"stack_slug,omitempty" url:"stack_slug,omitempty"`
+}
+
+type WorkspaceResponse struct {
+	AddConnectionURL string      `json:"add_connection_url"`
+	AddDatastreamURL string      `json:"add_datastream_url"`
+	ChangeURL        string      `json:"change_url"`
+	Datalake         string      `json:"datalake"`
+	Destination      interface{} `json:"destination"`
+	ExtractsURL      string      `json:"extracts_url"`
+	IssuesURL        string      `json:"issues_url"`
+	ID               int         `json:"id"`
+	Name             string      `json:"name"`
+	OverviewURL      string      `json:"overview_url"`
+	Parent           string      `json:"parent"`
+	ParentID         int         `json:"parent_id"`
+	Slug             string      `json:"slug"`
+	URL              string      `json:"url"`
+	Counts           struct {
+		Connections int `json:"connections"`
+		Datastreams int `json:"datastreams"`
+	} `json:"counts"`
+	Permissions struct {
+		IsCreator           bool `json:"isCreator"`
+		IsDatastreamManager bool `json:"isDatastreamManager"`
+		IsViewer            bool `json:"isViewer"`
+	} `json:"permissions"`
+	DefaultManageExtractNames bool   `json:"default_manage_extract_names"`
+	Updated                   string `json:"updated"`
+	Created                   string `json:"created"`
+}
+
+func (client *Client) CreateWorkspace(req CreateWorkspaceRequest) (*WorkspaceResponse, error) {
+	path, _ := url.JoinPath("stacks", "/")
+
+	payload, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.create(path, bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &WorkspaceResponse{}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (client *Client) ReadWorkspace(req ReadWorkspaceRequest) (*WorkspaceResponse, error) {
+	path, _ := url.JoinPath("stacks", req.StackSlug, "/")
+
+	body, err := client.read(path)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &WorkspaceResponse{}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (client *Client) UpdateWorkspace(req UpdateWorkspaceRequest) (*WorkspaceResponse, error) {
+	path, _ := url.JoinPath("stacks", req.StackSlug, "/")
+
+	payload, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.update(path, bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &WorkspaceResponse{}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (client *Client) DeleteWorkspace(req DeleteWorkspaceRequest) (*WorkspaceResponse, error) {
+	path, _ := url.JoinPath("stacks", req.StackSlug, "/")
+
+	payload, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := client.update(path, bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &WorkspaceResponse{}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
